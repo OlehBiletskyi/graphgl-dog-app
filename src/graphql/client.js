fe89480867116650df8f99e189b0afb6ab766bb5
OlehBiletskyi <ol.biletsky@gmail.com>
2022-03-24 17:24:01 +0200

    Updates

--- src/graphql/client.js
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-export const client = new ApolloClient({
-  uri: "https://71z1g.sse.codesandbox.io/",
-  cache: new InMemoryCache(),
-})
